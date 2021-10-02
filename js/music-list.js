297895a9471772e5c27b62a75d635ee6d4de74d8
widyasiregar <widyasiregarsongsandmusic@gmail.com>
2021-10-03 02:09:06 +0700

    first commit

---
+++ js/music-list.js
@@ -0,0 +1,97 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Dark Shadow",
+    artist: "Widya Siregar",
+    img: "Dark Shadow",
+    src: "music-1",
+  },
+
+  {
+    name: "La Playa Fiesta",
+    artist: "Widya Siregar",
+    img: "La Playa Fiesta",
+    src: "music-2",
+  },
+
+  {
+    name: "Mi Guapo Querido",
+    artist: "Widya Siregar",
+    img: "Mi Guapo Querido",
+    src: "music-3",
+  },
+
+  {
+    name: "Romantique Diner",
+    artist: "Widya Siregar",
+    img: "Romantique Diner",
+    src: "music-4",
+  },
+
+  {
+    name: "Along the Way to Jerusalem",
+    artist: "Widya Siregar",
+    img: "Along the Way to Jerusalem",
+    src: "music-5",
+  },
+
+  {
+    name: "Arabian Horse Rider",
+    artist: "Widya Siregar",
+    img: "Arabian Horse Rider",
+    src: "music-6",
+  },
+
+  {
+    name: "Beauty Nabawi",
+    artist: "Widya Siregar",
+    img: "music-7",
+    src: "music-7",
+  },
+
+  {
+    name: "Under the Sky of Taj Mahal",
+    artist: "Widya Siregar",
+    img: "music-8",
+    src: "music-8",
+  },
+
+  {
+    name: "La Silueta",
+    artist: "Widya Siregar",
+    img: "music-9",
+    src: "music-9",
+  },
+
+  {
+    name: "Lullaby for Prilla Vanda",
+    artist: "Widya Siregar",
+    img: "music-10",
+    src: "music-10",
+  },
+
+  {
+    name: "Istanbul Night",
+    artist: "Widya Siregar",
+    img: "music-11",
+    src: "music-11",
+  },
+
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
